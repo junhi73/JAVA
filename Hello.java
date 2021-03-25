99c32680320fb2afa035359fdacd80943cbad096
junhi73 <34299944+junhi73@users.noreply.github.com>
2021-03-25 17:12:18 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package day0114;
+
+//1. 소스코드작성.
+
+class Hello {
+	//Java Application : Java로 만들어진 단독으로 실행 가능한 프로그램.
+	public static void main(String[] args){
+		System.out.println("Hello! Java.");
+	} //main method : 혼자 실행가능하다. 
+
+ }//class
+
+
+//2. 저장 : Hello.java 
+
+//3. 컴파일 : javac 옵션 소스파일명.java 
+// DOS창과 파일간의 character set이 일치하지 않으면 한글로 인한 문제가 발생함. 
+// javac -encoding UTF-8 소스파일명.java 
+
+//4. 실행 : java Hello 
